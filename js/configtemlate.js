bb4e55161b9c604c79722c84d4f480aa19c4f655
K6NO <tamas.kenessey@gmail.com>
2017-04-01 23:52:13 +0200

    added configtemplate.js

---
+++ js/configtemlate.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // Put your info below and change the file name to config.js
+    // https://apps.twitter.com/
+    consumerKey:         '',
+    consumerSecret:      '',
+    accessToken:         '',
+    accessTokenSecret:  '',
+};
